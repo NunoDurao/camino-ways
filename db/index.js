eab5314637b29c64f2c88327523c119d5467b750
Renatopais <ren.tomar@gmail.com>
2023-05-18 12:56:24 +0100

    deploy

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/camino-ways";
+  `mongodb+srv://nunofdurao:${process.env.MONGODB_PASSWORD}@cluster0.h6ehxwx.mongodb.net/?retryWrites=true&w=majority` || "mongodb://127.0.0.1:27017/camino-ways";
 
 mongoose
   .connect(MONGO_URI)
@@ -16,4 +16,4 @@ mongoose
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
-  });
+  }); 
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -13,7 +13,55 @@ router.get("/profile", async (req, res) => {
       stage.author.equals(currentUser)
     );
 
-    res.render("profile", { user, userStages });
+    let portoVairao = [];
+    let vairaoBarcelos = [];
+    let barcelosPonteDeLima = [];
+    let ponteDeLimaRubiaes = [];
+    let rubiaesTui = [];
+    let tuiMos = [];
+    let mosPontevedra = [];
+    let pontevedraCaldasDeRei = [];
+    let caldasDeReiPadron = [];
+    let padronSantiagoDeCompostela = [];
+
+    for (let i = 0; i < userStages.length; i++) {
+      if (userStages[i].location === "Porto-Vairao") {
+        portoVairao.push(userStages[i]);
+      } else if (userStages[i].location === "Vairao-Barcelos") {
+        vairaoBarcelos.push(userStages[i]);
+      } else if (userStages[i].location === "Barcelos-Ponte de Lima") {
+        barcelosPonteDeLima.push(userStages[i]);
+      } else if (userStages[i].location === "Ponte de Lima-Rubiaes") {
+        ponteDeLimaRubiaes.push(userStages[i]);
+      } else if (userStages[i].location === "Rubiaes-Tui") {
+        rubiaesTui.push(userStages[i]);
+      } else if (userStages[i].location === "Tui-Mos") {
+        tuiMos.push(userStages[i]);
+      } else if (userStages[i].location === "Mos-Pontevedra") {
+        mosPontevedra.push(userStages[i]);
+      } else if (userStages[i].location === "Pontevedra-Caldas de Rei") {
+        pontevedraCaldasDeRei.push(userStages[i]);
+      } else if (userStages[i].location === "Caldas de Rei-Padron") {
+        caldasDeReiPadron.push(userStages[i]);
+      } else if (userStages[i].location === "Padron-Santiago de Compostela") {
+        padronSantiagoDeCompostela.push(userStages[i]);
+      }
+    } // <-- Move the closing bracket here
+
+    res.render("profile", {
+      user,
+      userStages,
+      portoVairao,
+      vairaoBarcelos,
+      barcelosPonteDeLima,
+      ponteDeLimaRubiaes,
+      rubiaesTui,
+      tuiMos,
+      mosPontevedra,
+      pontevedraCaldasDeRei,
+      caldasDeReiPadron,
+      padronSantiagoDeCompostela,
+    });
   } catch (error) {
     console.log(error);
   }
